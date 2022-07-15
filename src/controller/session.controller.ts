e785ab36c2295f83bb8d5ad7f15067429cb63b59
DevThomas <fremen1990@gmail.com>
2022-07-15 23:11:10 +0200

    FINAL API WORKING

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -6,45 +6,45 @@ import config from "config";
 
 export async function createUserSessionHandler(req:Request, res:Response){
 // validate user's password
-const user = await validatePassword(req.body)
-    if(!user){
-        return res.status(401).send("Invalid email or password")
-    }
+  const user = await validatePassword(req.body)
+  if(!user){
+    return res.status(401).send("Invalid email or password")
+  }
 
-    // create session
-const session = await createSession(user._id, req.get('user-agent') || "")
+  // create session
+  const session = await createSession(user._id, req.get('user-agent') || "")
 
-    // create access token
-const accessToken = signJwt(
-    {...user, session: session._id},
-    {expiresIn: config.get("accessTokenTtl")}//15min
-)
+  // create access token
+  const accessToken = signJwt(
+      {...user, session: session._id},
+      {expiresIn: config.get("accessTokenTtl")}//15min
+  )
 
-    //create a refresh token
-    const refreshToken = signJwt(
-        {...user, session: session._id},
-        {expiresIn: config.get("refreshTokenTtl")}//1y
-    )
+  //create a refresh token
+  const refreshToken = signJwt(
+      {...user, session: session._id},
+      {expiresIn: config.get("refreshTokenTtl")}//1y
+  )
 
-    //return access and refresh token
-return  res.send({accessToken, refreshToken})
+  //return access and refresh token
+  return  res.send({accessToken, refreshToken})
 }
 
 export async function getUserSessionsHandler(req: Request, res: Response) {
-    const userId = res.locals.user._id;
+  const userId = res.locals.user._id;
 
-    const sessions = await findSessions({ user: userId, valid: true });
+  const sessions = await findSessions({ user: userId, valid: true });
 
-    return res.send(sessions);
+  return res.send(sessions);
 }
 
 export async function deleteSessionHandler(req:Request, res:Response){
-    const sessionId=res.locals.user.session;
+  const sessionId=res.locals.user.session;
 
-    await updateSession({_id: sessionId}, {valid:false})
+  await updateSession({_id: sessionId}, {valid:false})
 
-    return res.send({
-        accessToken: null,
-        refreshToken:null
-    })
+  return res.send({
+    accessToken: null,
+    refreshToken:null
+  })
 }
